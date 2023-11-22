b63aed544136588642544124ed8cae9c9a4bb3ed
AGM54 <134453750+AGM54@users.noreply.github.com>
2023-11-21 19:25:21 -0600

    Update maquina.py

--- maquina.py
+++ maquina.py
@@ -5,3 +5,19 @@ class Tape:
         self.tape = list(input_string)
         self.head_position = 0
         self.blank_symbol = blank_symbol
+    def move_head(self, direction):
+        if direction == 'R':
+            self.head_position += 1
+            if self.head_position >= len(self.tape):
+                self.tape.append(self.blank_symbol)
+        elif direction == 'L':
+            self.head_position -= 1
+            if self.head_position < 0:
+                self.head_position = 0
+                self.tape.insert(0, self.blank_symbol)
+
+    def write_symbol(self, symbol):
+        self.tape[self.head_position] = symbol
+
+    def read_symbol(self):
+        return self.tape[self.head_position]
